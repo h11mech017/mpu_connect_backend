ac9386524c8ce98d24e68670dfd396413834a591
h11mech017 <jackylv95237@gmail.com>
2024-09-24 01:05:49 +0800

    test again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", setupRoutes());
+app.use("/api/", setupRoutes());
 
 admin.initializeApp({
   credential: admin.credential.cert({
