dfc334234c2d8f4f7a03af568a855bfb27a077b8
h11mech017 <jackylv95237@gmail.com>
2025-04-03 23:59:44 +0800

    Add return statement to fix student attendance tracking in CourseService

--- services/CourseService.js
+++ services/CourseService.js
@@ -1180,6 +1180,7 @@ export class CourseService {
                         Status: 'Present'
                     }
                 }
+                return student
             })
 
             await attendanceRef.update({
